22e4aba5552bd45ff19ce49552ba009af7334145
Titusz Pan <titusz.pan@gmail.com>
2021-08-30 15:26:32 +0200

    Fix instance hash empty byte or None.

--- iscc_core/instance.py
+++ iscc_core/instance.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-import os
-import io
 import blake3
 from typing import Optional
-from iscc_core.base import Data, Stream
-
-
-VERSION_INSTANCE_HASH = 0
-BUFFER_SIZE = os.getenv("ISCC_BUFFER_SIZE", io.DEFAULT_BUFFER_SIZE)
+from iscc_core.base import Data, Stream, INSTANCE_READ_SIZE
 
 
 def hash_instance(stream: Stream) -> bytes:
@@ -15,17 +9,19 @@ def hash_instance(stream: Stream) -> bytes:
 
 
 def hash_instance_v0(stream: Stream) -> bytes:
-    data = stream.read(BUFFER_SIZE)
-    hasher = InstanceHasher(data)
+    hasher = InstanceHasherV0()
+    data = stream.read(INSTANCE_READ_SIZE)
     while data:
-        data = stream.read(BUFFER_SIZE)
         hasher.push(data)
+        data = stream.read(INSTANCE_READ_SIZE)
     return hasher.digest()
 
 
 class InstanceHasherV0:
     def __init__(self, data: Optional[Data] = None):
-        self.hasher = blake3.blake3(data)
+        self.hasher = blake3.blake3()
+        data = data or b''
+        self.push(data)
 
     def push(self, data: Data) -> None:
         self.hasher.update(data)
