593cc92c90a252f0b3fd534ad0093170dbf33e7b
Titusz Pan <titusz.pan@gmail.com>
2021-08-30 14:23:28 +0200

    Add data hash module.

--- iscc_core/data.py
+++ iscc_core/data.py
@@ -1,25 +1,61 @@
 # -*- coding: utf-8 -*-
-import io
 from typing import Optional
 from iscc_core.base import Data, Stream
+from iscc_core.cdc import data_chunks
+from iscc_core.base import CDC_READ_SIZE
+from iscc_core.minhash import minhash_256
+import xxhash
 
 
 def hash_data(stream: Stream) -> bytes:
-    pass
+    return hash_data_v0(stream)
 
 
-class DataHasher:
-    def __init__(self, data: Optional[Data]):
-        self.buffer = b""
-        if data:
+def hash_data_v0(stream: Stream) -> bytes:
+    data = stream.read(CDC_READ_SIZE)
+    hasher = DataHasherV0(data)
+
+    data = stream.read(CDC_READ_SIZE)
+    while data:
+        hasher.push(data)
+        data = stream.read(CDC_READ_SIZE)
+
+    return hasher.digest()
+
+
+class DataHasherV0:
+    def __init__(self, data: Optional[Data] = None):
+        self.chunk_features = []
+        self.chunk_sizes = []
+        self.tail = None
+        if data is not None:
             self.push(data)
 
     def push(self, data: Optional[Data]):
-        pass
+
+        if self.tail:
+            data = self.tail + data
+
+        for chunk in data_chunks(data):
+            self.chunk_sizes.append(len(chunk))
+            self.chunk_features.append(xxhash.xxh32_intdigest(chunk))
+
+        # Last chunk may not be final
+        self.chunk_features = self.chunk_features[:-1]
+        self.chunk_sizes = self.chunk_sizes[:-1]
+        self.tail = chunk
+
+    def digest(self):
+        chunk_features = self.chunk_features
+        chunk_sizes = self.chunk_sizes
+        if self.tail is not None:
+            chunk_features.append(xxhash.xxh32_intdigest(self.tail))
+            chunk_sizes.append(len(self.tail))
+            self.tail = None
+        return minhash_256(chunk_features)
 
 
-if __name__ == "__main__":
-    pass
+DataHasher = DataHasherV0
 
-d = io.BytesIO()
-print(d.read())
+
+print(DataHasher(b"\x00").digest().hex())
