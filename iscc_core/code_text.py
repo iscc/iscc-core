790c8bb5f9f17b2278d9f6233ff9e648b74a718b
Titusz Pan <titusz.pan@gmail.com>
2021-09-02 19:48:48 +0200

    Refactors sliding_window and add tests.

--- iscc_core/code_text.py
+++ iscc_core/code_text.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from typing import Generator, Sequence
 import xxhash
 from iscc_core.minhash import minhash_256
 from iscc_core.base import TEXT_NGRAM_SIZE
+from iscc_core.utils import sliding_window
 
 
 def hash_text(text: str) -> bytes:
@@ -22,18 +22,3 @@ def hash_text_v0(text: str) -> bytes:
     features = [xxhash.xxh32_intdigest(s.encode("utf-8")) for s in ngrams]
     hash_digest = minhash_256(features)
     return hash_digest
-
-
-def sliding_window(seq, width):
-    # type: (Sequence, int) -> Generator
-    """
-    Generate a sequence of equal "width" slices each advancing by one elemnt.
-    All types that have a length and can be sliced are supported (list, tuple, str ...).
-    The result type matches the type of the input sequence.
-    Fragment slices smaller than the width at the end of the sequence are not produced.
-    If "witdh" is smaller than the input sequence than one element will be returned that
-    is shorter than the requested width.
-    """
-    assert width >= 2, "Sliding window width must be 2 or bigger."
-    idx = range(max(len(seq) - width + 1, 1))
-    return (seq[i : i + width] for i in idx)
---
+++ iscc_core/utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from typing import Generator, Sequence
+
+
+def sliding_window(seq, width):
+    # type: (Sequence, int) -> Generator
+    """
+    Generate a sequence of equal "width" slices each advancing by one elemnt.
+    All types that have a length and can be sliced are supported (list, tuple, str ...).
+    The result type matches the type of the input sequence.
+    Fragment slices smaller than the width at the end of the sequence are not produced.
+    If "witdh" is smaller than the input sequence than one element will be returned that
+    is shorter than the requested width.
+    """
+    assert width >= 2, "Sliding window width must be 2 or bigger."
+    idx = range(max(len(seq) - width + 1, 1))
+    return (seq[i : i + width] for i in idx)
