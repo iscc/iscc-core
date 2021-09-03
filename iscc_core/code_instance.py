0c746fc6229d642b36350a933cdb87e470f61e4a
Titusz Pan <titusz.pan@gmail.com>
2021-09-03 20:54:24 +0200

    Add code_instance.

--- iscc_core/code_instance.py
+++ iscc_core/code_instance.py
@@ -2,13 +2,32 @@
 import blake3
 from typing import Optional
 from iscc_core.base import Data, Stream, INSTANCE_READ_SIZE
+from iscc_core import codec
 
 
-def hash_instance(stream: Stream) -> bytes:
-    return hash_instance_v0(stream)
+def code_instance(stream, bits=64):
+    # type: (Stream, int) -> str
+    """Create an ISCC Instance-Code with the latest standard algorithm"""
+    return code_instance_v0(stream, bits)
 
 
-def hash_instance_v0(stream: Stream) -> bytes:
+def code_instance_v0(stream, bits=64):
+    # type: (Stream, int) -> str
+    """Create an ISCC Instance-Code with algorithm v0"""
+    digest = hash_instance_v0(stream)
+    instance_code = codec.encode_component(
+        mtype=codec.MT.INSTANCE,
+        stype=codec.ST.NONE,
+        version=codec.VS.V0,
+        length=bits,
+        digest=digest,
+    )
+    return instance_code
+
+
+def hash_instance_v0(stream):
+    # type: (Stream) -> bytes
+    """Create 256-bit hash for Instance-Code"""
     hasher = InstanceHasherV0()
     data = stream.read(INSTANCE_READ_SIZE)
     while data:
@@ -18,15 +37,18 @@ def hash_instance_v0(stream: Stream) -> bytes:
 
 
 class InstanceHasherV0:
-    def __init__(self, data: Optional[Data] = None):
+    def __init__(self, data=None):
+        # type: (Optional[Data]) -> None
         self.hasher = blake3.blake3()
         data = data or b""
         self.push(data)
 
-    def push(self, data: Data) -> None:
+    def push(self, data):
+        # type: (Data) -> None
         self.hasher.update(data)
 
     def digest(self):
+        # type: () -> bytes
         return self.hasher.digest()
 
 
--- tests/test_code_instance.py
+++ tests/test_code_instance.py
@@ -2,15 +2,30 @@ from io import BytesIO
 from iscc_core import code_instance
 
 
-def test_hash_instance_empty():
+def test_hash_instance_v0_empty():
     assert (
-        code_instance.hash_instance(BytesIO(b"")).hex()
+        code_instance.hash_instance_v0(BytesIO(b"")).hex()
         == "af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262"
     )
 
 
-def test_hash_instance_zero():
+def test_hash_instance_v0_zero():
     assert (
-        code_instance.hash_instance(BytesIO(b"\x00")).hex()
+        code_instance.hash_instance_v0(BytesIO(b"\x00")).hex()
         == "2d3adedff11b61f14c886e35afa036736dcd87a74d27b5c1510225d0f592e213"
+    )
+
+
+def test_code_instance_v0_empty_default():
+    assert code_instance.code_instance_v0(BytesIO(b"")) == "IAA26E2JXH27TING"
+
+
+def test_code_instance_v0_zero_default():
+    assert code_instance.code_instance_v0(BytesIO(b"\x00")) == "IAAS2OW637YRWYPR"
+
+
+def test_code_instance_v0_hello_world_128():
+    assert (
+        code_instance.code_instance_v0(BytesIO(b"hello world"), 128)
+        == "IAB5OSMB56TQUDEIBOGYYGMF2B25W"
     )
