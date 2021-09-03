5e4a1c4c448f4f3b216773409d9f716cc97d82bf
Titusz Pan <titusz.pan@gmail.com>
2021-09-03 16:34:32 +0200

    Add codec module.

---
+++ iscc_core/codec.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+The iscc.codec module provides encoding, decoding and transcoding related functions.
+
+ISCC Component Structure:
+
+Header:
+    <type> <subtype> <version> <length> each coded as a variable-length 4-bit sequence.
+Body:
+    <hash-digest> with number of bits as indicated by <length>
+"""
+import enum
+import math
+from typing import Tuple
+from bitarray import bitarray
+from bitarray.util import int2ba, ba2int
+from base64 import b32encode, b32decode
+
+try:
+    from pybase64 import urlsafe_b64encode, urlsafe_b64decode
+except ImportError:
+    from base64 import urlsafe_b64encode, urlsafe_b64decode
+
+
+class MT(enum.IntEnum):
+    """ISCC Main Types"""
+
+    META = 0
+    SEMANTIC = 1
+    CONTENT = 2
+    DATA = 3
+    INSTANCE = 4
+    ISCC = 5
+
+
+class ST(enum.IntEnum):
+    """Generic SubTypes"""
+
+    NONE = 0
+
+
+class ST_CC(enum.IntEnum):
+    """SubTypes for Content Codes"""
+
+    TEXT = 0
+    IMAGE = 1
+    AUDIO = 2
+    VIDEO = 3
+    GENERIC = 4
+
+
+class VS(enum.IntEnum):
+    """ISCC code Versions"""
+
+    V0 = 0
+
+
+class LN(enum.IntEnum):
+    """ISCC lenth in bits"""
+
+    L32 = 32
+    L64 = 64
+    L128 = 128
+    L256 = 256
+
+
+def write_header(mtype: int, stype: int, version: int = 0, length: int = 64) -> bytes:
+    """
+    Encodes header values with nibble-sized variable-length encoding.
+    The result is minimum 2 and maximum 8 bytes long. If the final count of nibbles
+    is uneven it is padded with 4-bit `0000` at the end.
+    """
+    assert length >= 32 and not length % 32, "Length must be a multiple of 32"
+    length = (length // 32) - 1
+    header = bitarray()
+    for n in (mtype, stype, version, length):
+        header += _write_varnibble(n)
+    # Append zero-padding if required (right side, least significant bits).
+    header.fill()
+    return header.tobytes()
+
+
+def read_header(data: bytes) -> Tuple[int, int, int, int, bytes]:
+    """
+    Decodes varnibble encoded header and returns it together with remaining bytes.
+    :returns: (type, subtype, version, length, remaining bytes)
+    """
+    result = []
+    ba = bitarray()
+    ba.frombytes(data)
+    data = ba
+    for x in range(3):
+        value, data = _read_varnibble(data)
+        result.append(value)
+
+    # interpret length value
+    length, data = _read_varnibble(data)
+
+    bit_length = (length + 1) * 32
+    result.append(bit_length)
+
+    # Strip 4-bit padding if required
+    if len(data) % 8 and data[:4] == bitarray("0000"):
+        data = data[4:]
+
+    result.append(data.tobytes())
+
+    return tuple(result)
+
+
+def encode_base32(data: bytes) -> str:
+    """
+    Standard RFC4648 base32 encoding without padding.
+    """
+    return b32encode(data).decode("ascii").rstrip("=")
+
+
+def decode_base32(code: str) -> bytes:
+    """
+    Standard RFC4648 base32 decoding without padding and with casefolding.
+    """
+    # python stdlib does not support base32 without padding, so we have to re-pad.
+    cl = len(code)
+    pad_length = math.ceil(cl / 8) * 8 - cl
+
+    return bytes(b32decode(code + "=" * pad_length, casefold=True))
+
+
+def _write_varnibble(n: int) -> bitarray:
+    """
+    Writes integer to variable length sequence of 4-bit chunks.
+    Variable-length encoding scheme:
+    ------------------------------------------------------
+    | prefix bits | nibbles | data bits | unsigned range |
+    | ----------- | ------- | --------- | -------------- |
+    | 0           | 1       | 3         | 0 - 7          |
+    | 10          | 2       | 6         | 8 - 71         |
+    | 110         | 3       | 9         | 72 - 583       |
+    | 1110        | 4       | 12        | 584 - 4679     |
+    ------------------------------------------------------
+    """
+    if 0 <= n < 8:
+        return int2ba(n, length=4)
+    elif 8 <= n < 72:
+        return bitarray("10") + int2ba(n - 8, length=6)
+    elif 72 <= n < 584:
+        return bitarray("110") + int2ba(n - 72, length=9)
+    elif 584 <= n < 4680:
+        return bitarray("1110") + int2ba(n - 584, length=12)
+    else:
+        raise ValueError("Value must be between 0 and 4679")
+
+
+def _read_varnibble(b: bitarray) -> Tuple[int, bitarray]:
+    """Reads first varnibble, returns its integer value and remaining bits."""
+
+    bits = len(b)
+
+    if bits >= 4 and b[0] == 0:
+        return ba2int(b[:4]), b[4:]
+    if bits >= 8 and b[1] == 0:
+        return ba2int(b[2:8]) + 8, b[8:]
+    if bits >= 12 and b[2] == 0:
+        return ba2int(b[3:12]) + 72, b[12:]
+    if bits >= 16 and b[3] == 0:
+        return ba2int(b[4:16]) + 584, b[16:]
+
+    raise ValueError("Invalid bitarray")
+
+
+def clean(iscc: str) -> str:
+    """Cleanup ISCC String.
+
+    Removes leading scheme and dashes.
+    """
+    return iscc.split(":")[-1].strip().replace("-", "")
---
+++ tests/test_codec.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+import pytest
+from iscc_core import codec as c
+from bitarray import bitarray as ba
+
+
+def test_main_type():
+    assert isinstance(c.MT.META, int)
+    assert c.MT.META == 0
+
+
+def test_write_header():
+    with pytest.raises(AssertionError):
+        c.write_header(0, 0, 0, 0)
+    assert c.write_header(0, 0, 0, 32) == bytes([0b0000_0000, 0b0000_0000])
+    assert c.write_header(1, 0, 0, 32) == bytes([0b0001_0000, 0b0000_0000])
+    assert c.write_header(7, 1, 1, 64) == bytes([0b0111_0001, 0b0001_0001])
+    assert c.write_header(8, 1, 1, 64) == bytes([0b1000_0000, 0b0001_0001, 0b0001_0000])
+    assert c.write_header(8, 8, 1, 64) == bytes([0b1000_0000, 0b1000_0000, 0b0001_0001])
+
+
+def test_read_header():
+    rh = c.read_header
+    assert rh(bytes([0b0000_0000, 0b0000_0000])) == (0, 0, 0, 32, b"")
+    assert rh(bytes([0b0000_0000, 0b0000_0000, 0b0000_0000])) == (0, 0, 0, 32, b"\x00")
+    assert rh(bytes([0b0001_0000, 0b0000_0000])) == (1, 0, 0, 32, b"")
+    assert rh(bytes([0b0111_0001, 0b0001_0001])) == (7, 1, 1, 64, b"")
+    assert rh(bytes([0b1000_0000, 0b0001_0001, 0b0001_0000])) == (8, 1, 1, 64, b"")
+    assert rh(bytes([0b1000_0000, 0b1000_0000, 0b0001_0001])) == (8, 8, 1, 64, b"")
+
+
+def test_encode_base32():
+    assert c.encode_base32(b"") == ""
+    assert c.encode_base32(b"f") == "MY"
+    assert c.encode_base32(b"fo") == "MZXQ"
+    assert c.encode_base32(b"foo") == "MZXW6"
+    assert c.encode_base32(b"foob") == "MZXW6YQ"
+    assert c.encode_base32(b"fooba") == "MZXW6YTB"
+    assert c.encode_base32(b"foobar") == "MZXW6YTBOI"
+
+
+def test_decode_base32():
+    assert c.decode_base32("") == b""
+    assert c.decode_base32("MY") == b"f"
+    assert c.decode_base32("My") == b"f"
+    assert c.decode_base32("my") == b"f"
+    assert c.decode_base32("MZXQ") == b"fo"
+    assert c.decode_base32("MZXW6") == b"foo"
+    assert c.decode_base32("MZXW6YQ") == b"foob"
+    assert c.decode_base32("MZXW6YTB") == b"fooba"
+    assert c.decode_base32("MZXW6YTBOI") == b"foobar"
+
+
+def test_write_varnibble():
+    with pytest.raises(ValueError):
+        c._write_varnibble(-1)
+    assert c._write_varnibble(0) == ba("0000")
+    assert c._write_varnibble(7) == ba("0111")
+    assert c._write_varnibble(8) == ba("10000000")
+    assert c._write_varnibble(9) == ba("10000001")
+    assert c._write_varnibble(71) == ba("10111111")
+    assert c._write_varnibble(72) == ba("110000000000")
+    assert c._write_varnibble(73) == ba("110000000001")
+    assert c._write_varnibble(583) == ba("110111111111")
+    assert c._write_varnibble(584) == ba("1110000000000000")
+    assert c._write_varnibble(4679) == ba("1110111111111111")
+    with pytest.raises(ValueError):
+        c._write_varnibble(4680)
+    with pytest.raises(TypeError):
+        c._write_varnibble(1.0)
+
+
+def test_read_varnibble():
+    with pytest.raises(ValueError):
+        c._read_varnibble(ba("0"))
+    with pytest.raises(ValueError):
+        c._read_varnibble(ba("1"))
+    with pytest.raises(ValueError):
+        c._read_varnibble(ba("011"))
+    with pytest.raises(ValueError):
+        c._read_varnibble(ba("100"))
+    assert c._read_varnibble(ba("0000")) == (0, ba())
+    assert c._read_varnibble(ba("000000")) == (0, ba("00"))
+    assert c._read_varnibble(ba("0111")) == (7, ba())
+    assert c._read_varnibble(ba("01110")) == (7, ba("0"))
+    assert c._read_varnibble(ba("01111")) == (7, ba("1"))
+    assert c._read_varnibble(ba("10000000")) == (8, ba())
+    assert c._read_varnibble(ba("10000001")) == (9, ba())
+    assert c._read_varnibble(ba("10000001110")) == (9, ba("110"))
+    assert c._read_varnibble(ba("10111111")) == (71, ba())
+    assert c._read_varnibble(ba("101111110")) == (71, ba("0"))
+    assert c._read_varnibble(ba("110000000000")) == (72, ba())
+    assert c._read_varnibble(ba("11000000000010")) == (72, ba("10"))
+    assert c._read_varnibble(ba("110000000001")) == (73, ba())
+    assert c._read_varnibble(ba("110000000001010")) == (73, ba("010"))
+    assert c._read_varnibble(ba("110111111111")) == (583, ba())
+    assert c._read_varnibble(ba("1101111111111010")) == (583, ba("1010"))
+    assert c._read_varnibble(ba("1110000000000000")) == (584, ba())
+    assert c._read_varnibble(ba("111000000000000001010")) == (584, ba("01010"))
+    assert c._read_varnibble(ba("1110111111111111")) == (4679, ba())
+    assert c._read_varnibble(ba("1110111111111111101010")) == (4679, ba("101010"))
+
+
+def test_iscc_clean():
+    assert c.clean("somecode") == "somecode"
+    assert c.clean("ISCC: SOME-CODE") == "SOMECODE"
+    assert c.clean(" SOMECODE ") == "SOMECODE"
+    assert c.clean("ISCC:") == ""
