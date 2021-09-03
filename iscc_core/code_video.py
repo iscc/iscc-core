c31e3b11902976af80bef9d24944b734ad86930b
Titusz Pan <titusz.pan@gmail.com>
2021-09-03 17:32:19 +0200

    Add code_video.

--- iscc_core/code_video.py
+++ iscc_core/code_video.py
@@ -4,37 +4,31 @@ ISCC Content-Code Video
 
 The Content-Code Video is generated from MPEG-7 Video Frame Signatures.
 Frame Signatures can be extracted with ffmpeg (see: https://www.ffmpeg.org/).
-
-
 """
 from typing import Sequence, Tuple
 from iscc_core.wtahash import wtahash
 from iscc_core import codec
 
 
-def code_video(frame_signatures: Sequence[Tuple[int]], bits=64) -> str:
+def code_video(frame_signatures, bits=64):
+    # type: (Sequence[Tuple[int]], int) -> str
     """Create an ISCC Content-Code Video with the latest standard algorithm."""
     return code_video_v0(frame_signatures, bits)
 
 
-def code_video_v0(frame_signatures: Sequence[Tuple[int]], bits=64) -> str:
+def code_video_v0(frame_signatures, bits=64):
+    # type: (Sequence[Tuple[int]], int) -> str
     """Create an ISCC Content-Code Video with algorithm v0."""
-    nbytes = bits // 8
-    header = codec.write_header(
-        codec.MT.CONTENT, codec.ST_CC.VIDEO, version=0, length=bits
+    digest = hash_video_v0(frame_signatures)
+    video_code = codec.encode_component(
+        codec.MT.CONTENT, codec.ST_CC.VIDEO, version=0, length=bits, digest=digest
     )
-    digest = hash_audio_v0(cv)[:nbytes]
-    audio_code = codec.encode_base32(header + digest)
-    return audio_code
+    return video_code
 
 
-def hash_video(frame_signatures: Sequence[Tuple[int]]) -> bytes:
-    """Compute video hash (latest algorithm)."""
-    return hash_video_v0(frame_signatures)
-
-
-def hash_video_v0(frame_signatures: Sequence[Tuple[int]]) -> bytes:
-    """Compute video hash v0 from MP7 frame signatures."""
+def hash_video_v0(frame_signatures):
+    # type: (Sequence[Tuple[int]]) -> bytes
+    """Compute 256-bit video hash v0 from MP7 frame signatures."""
     sigs = set(frame_signatures)
     vecsum = [sum(col) for col in zip(*sigs)]
     video_hash_digest = wtahash(vecsum)
--- tests/test_code_video.py
+++ tests/test_code_video.py
@@ -2,23 +2,45 @@
 from iscc_core import code_video
 
 
-def test_hash_video_0_features():
-    assert code_video.hash_video([tuple([0] * 380)]) == b"\x00" * 32
+def test_hash_video_v0_features():
+    assert code_video.hash_video_v0([tuple([0] * 380)]) == b"\x00" * 32
 
 
-def test_hash_video_range():
+def test_hash_video_v0_range():
     frame_vectors = [tuple(range(380))]
     assert (
-        code_video.hash_video(frame_vectors).hex()
+        code_video.hash_video_v0(frame_vectors).hex()
         == "528f91431f7c4ad26932fc073a28cac93f21a3071a152fc2925bdaed1d190061"
     )
 
 
-def test_hash_video_multiple_framevectors():
+def test_hash_video_v0_multiple_framevectors():
     fa = tuple([0, 1, 0, 2, 1] * 76)
     fb = tuple([1, 2, 1, 0, 2] * 76)
     frame_vectors = [fa, fb]
     assert (
-        code_video.hash_video(frame_vectors).hex()
+        code_video.hash_video_v0(frame_vectors).hex()
         == "9230d219501e00f42664b4bd206b000c98488635b0b03c010010ee00aaf93e43"
+    )
+
+
+def test_code_video_v0_features():
+    assert code_video.code_video_v0([tuple([0] * 380)]) == "EMAQAAAAAAAAAAAA"
+
+
+def test_code_video_v0_range_128():
+    frame_vectors = [tuple(range(380))]
+    assert (
+        code_video.code_video_v0(frame_vectors, bits=128)
+        == "EMBVFD4RIMPXYSWSNEZPYBZ2FDFMS"
+    )
+
+
+def test_code_video_v0_multiple_framevectors_256():
+    fa = tuple([0, 1, 0, 2, 1] * 76)
+    fb = tuple([1, 2, 1, 0, 2] * 76)
+    frame_vectors = [fa, fb]
+    assert (
+        code_video.code_video_v0(frame_vectors, bits=256)
+        == "EMDZEMGSDFIB4AHUEZSLJPJANMAAZGCIQY23BMB4AEABB3QAVL4T4QY"
     )
