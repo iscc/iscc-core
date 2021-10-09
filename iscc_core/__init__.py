899c27044e4830e45440e432b013c66b3fd948db
Titusz Pan <titusz.pan@gmail.com>
2021-10-09 13:02:43 +0200

    Import main functions to package level.

--- iscc_core/__init__.py
+++ iscc_core/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.1.0"
+
+from iscc_core.code_meta import gen_meta_code
+from iscc_core.code_content_text import gen_text_code
+from iscc_core.code_content_image import gen_image_code
+from iscc_core.code_content_audio import gen_audio_code
+from iscc_core.code_content_video import gen_video_code
+from iscc_core.code_data import gen_data_code
+from iscc_core.code_instance import gen_instance_code
