f2d99af6ee73bfa8dfe2d203a65df3f7665a77f6
Titusz Pan <titusz.pan@gmail.com>
2021-11-11 17:51:31 +0100

    Flesh out Meta-Code module.

--- iscc_core/code_meta.py
+++ iscc_core/code_meta.py
@@ -1,73 +1,143 @@
 # -*- coding: utf-8 -*-
 """
-ISCC Meta-Code
+The Meta-Code is the first component of a fully qualified ISCC. It is calculated as a
+similarity preserving hash from the metadata of a digital asset. The purpose of the
+Meta-Code is the discovery of digital assets with similar metadata or spelling mistakes.
+
+The metadata we supply to the algorithm is called *seed metadata*.
+
+*Seed metadata* is composed of a `title` and an optional generic `extra`-field that
+contains descriptive, industry-sector or use-case specific metadata in textual or
+binary form (e.g. file headers). We do not prescribe a particular schema.
 """
 from more_itertools import interleave, sliced
-from iscc_core import codec
-from iscc_core.base import META_NGRAM_SIZE
+from iscc_core.code_content_text import normalize_text
+from iscc_core.codec import MT, ST_MC, VS, encode_component
 from iscc_core.utils import sliding_window
 from iscc_core.simhash import similarity_hash
+from iscc_core.options import opts
 from blake3 import blake3
+from typing import Union
 
 
-def gen_meta_code(title, extra="", bits=64):
-    # type: (str, str, int) -> str
+def gen_meta_code(title, extra=None, bits=opts.meta_bits):
+    # type: (str, Union[str,bytes,None], int) -> str
     """Create an ISCC Meta-Code using the latest standard algorithm.
 
-    :param str title: Title of the work manifested in the digital asset.
-    :param str extra: Optional additional text for disambiguation of the work.
-    :param int bits: Bit-length of Meta-Hash.
-    :return: ISCC Meta-Code.
+    Applications that generate ISCCs should prioritize explicitly passed `title`
+    information. If not available they shall try to extract a title form the the digital
+    asset itself. If extraction fails, the application shall resort to the filename
+    before falling back to an empty string.
+
+    Optional additional metadata may be supplied via the `extra`-field.
+    The input can be:
+
+    - A textual description of the identified work for disambiguation purposes
+    - Structured (JSON) metadata conforming to an industry specific metadata schema
+    - Raw bitstream file headers automatically extracted binary file headers
+    - A pre-existing industry-specific identifier string
+
+    !!! note
+        It is recommended to use the minimal metadata required to disambiguate the work
+        manifested by the digital asset.
+
+    :param str title: Title of the work manifested by the digital asset
+    :param Union[str,bytes,None] extra: Optional metadata for disambiguation
+    :param int bits: Bit-length of resulting Meta-Code (multiple of 64)
+    :return: ISCC Meta-Code
     :rtype: str
     """
-    return gen_meta_code_v0(title, extra, bits)
+    return gen_meta_code_v0(title, extra=extra, bits=bits)
 
 
-def gen_meta_code_v0(title, extra="", bits=64):
-    # type: (str, str, int) -> str
+def gen_meta_code_v0(title, extra=None, bits=opts.meta_bits):
+    # type: (str, Union[str,bytes,None], int) -> str
     """Create an ISCC Meta-Code with the algorithm version 0.
 
-    :param str title: Title of the work manifested in the digital asset.
-    :param str extra: Optional additional text for disambiguation of the work.
-    :param int bits: Bit-length of Meta-Hash.
-    :return: ISCC Meta-Code.
+    :param str title: Title of the work manifested by the digital asset
+    :param Union[str,bytes,None] extra: Optional metadata for disambiguation
+    :param int bits: Bit-length of resulting Meta-Code (multiple of 64)
+    :return: ISCC Meta-Code
     :rtype: str
     """
+
+    # 1. Normalize title
+    title = "" if title is None else title
+    title = normalize_text(title)
+    title = trim_text(title, opts.meta_trim_title)
+
+    # 2. Normalize extra and set Meta-Code SubType
+    if extra in (None, ""):
+        extra = None
+        stype = ST_MC.TEXT
+    elif isinstance(extra, str):
+        extra = normalize_text(extra)
+        extra = trim_text(extra, opts.meta_trim_extra)
+        stype = ST_MC.TEXT
+    elif isinstance(extra, bytes):
+        extra = extra[: opts.meta_trim_extra]
+        stype = ST_MC.BYTES
+    else:
+        raise ValueError("parameter `extra` must be of type str or bytes!")
+
     digest = hash_meta_v0(title, extra)
-    meta_code = codec.encode_component(
-        mtype=codec.MT.META,
-        stype=codec.ST.NONE,
-        version=codec.VS.V0,
+    meta_code = encode_component(
+        mtype=MT.META,
+        stype=stype,
+        version=VS.V0,
         length=bits,
         digest=digest,
     )
     return meta_code
 
 
-def hash_meta_v0(title, extra="", ngram_size=META_NGRAM_SIZE):
-    # type: (str, str, int) -> bytes
-    """Calculate simmilarity preserving 256-bit hash digest from metadata.
+def hash_meta_v0(title, extra=None):
+    # type: (str, Union[str,bytes,None]) -> bytes
+    """Calculate simmilarity preserving 256-bit hash digest for provided metadata.
 
-    Text input should be stripped of markup, normalized and trimmed before hashing.
-    If the extra field is supplied we create separate hashes for title and extra and
-    interleave them in 32-bit chunks.
+    Textual input should be stripped of markup, normalized and trimmed before hashing.
+    Json metadata should be normalized with
+    [JCS](https://tools.ietf.org/id/draft-rundgren-json-canonicalization-scheme-00.html)
 
-    :param str title: Title of the work manifested in the digital asset.
-    :param str extra: Optional additional text for disambiguation of the work.
-    :param int ngram_size: Number of characters used for sliding tokenization (ngram).
-    :return: 256-bit simhash byte digest for Meta-Code.
+    !!! note
+        If the `extra` field is supplied we create separate hashes for title and extra
+        and interleave them in 32-bit chunks. The processign algorithms and the subtype
+        of the generated hash digest depends on the type the `extra` input.
+        If the input is a **text string** (str) the processing unit is a character
+        (possibly multibyte) and the resulting hash type is **ST_MC.TEXT (0)**. If the
+        input is **bytes** the processing is done bytewise and the resulting hash type
+        is **ST_MC.BYTES (1)**.
+
+    :param str title: Title of the work manifested in the digital asset
+    :param Union[str,bytes,None] extra: Additional metadata for disambiguation
+    :return: 256-bit simhash digest for Meta-Code of SubType ST_MC.TEXT or ST.MC_BYTES
     :rtype: bytes
     """
     title = title.lower()
-    title_n_grams = sliding_window(title, width=ngram_size)
+    title_n_grams = sliding_window(title, width=opts.meta_ngram_size_title)
     title_hash_digests = [blake3(s.encode("utf-8")).digest() for s in title_n_grams]
     simhash_digest = similarity_hash(title_hash_digests)
 
-    # Simhash extra metadata
-    if extra:
-        extra = extra.lower()
-        extra_n_grams = sliding_window(extra, width=ngram_size)
-        extra_hash_digests = [blake3(s.encode("utf-8")).digest() for s in extra_n_grams]
+    if extra in (None, ""):
+        return simhash_digest
+    else:
+        # Augment with interleaved hash for extra metadata
+        if isinstance(extra, bytes):
+            # Raw bytes are handled per byte
+            extra_n_grams = sliding_window(
+                extra, width=opts.meta_ngram_size_extra_binary
+            )
+            extra_hash_digests = [blake3(ngram).digest() for ngram in extra_n_grams]
+        elif isinstance(extra, str):
+            # Text is lower cased and handled per character (multibyte)
+            extra = extra.lower()
+            extra_n_grams = sliding_window(extra, width=opts.meta_ngram_size_extra_text)
+            extra_hash_digests = [
+                blake3(s.encode("utf-8")).digest() for s in extra_n_grams
+            ]
+        else:
+            raise ValueError("parameter `extra` must be of type str or bytes!")
+
         extra_simhash_digest = similarity_hash(extra_hash_digests)
 
         # Interleave first half of title and extra simhashes in 32-bit chunks
@@ -80,4 +150,10 @@ def hash_meta_v0(title, extra="", ngram_size=META_NGRAM_SIZE):
 
         simhash_digest = bytes(simhash_digest)
 
-    return simhash_digest
+        return simhash_digest
+
+
+def trim_text(text, nbytes):
+    # type: (str, int) -> str
+    """Trim text such that its utf-8 encoded size does not exceed `nbytes`."""
+    return text.encode("utf-8")[:nbytes].decode("utf-8", "ignore").strip()
--- tests/test_code_meta.py
+++ tests/test_code_meta.py
@@ -2,8 +2,50 @@
 from iscc_core import code_meta
 
 
-def test_hash_meta_v0_empty():
+def test_gen_meta_code_title_none():
+    m = code_meta.gen_meta_code(None)
+    assert m == "AAA26E2JXH27TING"
+
+
+def test_gen_meta_code_title_empty_str():
+    m = code_meta.gen_meta_code("")
+    assert m == "AAA26E2JXH27TING"
+
+
+def test_gen_meta_code_title_with_extra():
+    empty = "AAA26E2JXH27TING"
+    m = code_meta.gen_meta_code(None, None)
+    assert m == empty
+    m = code_meta.gen_meta_code(None, "")
+    assert m == empty
+    m = code_meta.gen_meta_code("", None)
+    assert m == empty
+    m = code_meta.gen_meta_code("", "")
+    assert m == empty
+
+
+def test_gen_meta_code_text_vs_bytes():
+    m = code_meta.gen_meta_code("", "hello world")
+    assert m == "AAA26E2JXH733ZNM"
+    m = code_meta.gen_meta_code("", b"hello world")
+    assert m == "AEA26E2JXH733ZNM"
+    m = code_meta.gen_meta_code("", b"\x80")  # not utf-8 decodable
+    assert m == "AEA26E2JXG56NKPV"
+
+
+def test_gen_meta_code_title_only():
+    m = code_meta.gen_meta_code("Hello World")
+    assert m == "AAA77PPFVS6JDUQB"
+
+
+def test_hash_meta_v0_empty_title_str():
     m = code_meta.hash_meta_v0("")
+    assert len(m) == 32
+    assert m.hex() == "af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262"
+
+
+def test_hash_meta_v0_empty_title_extra_str():
+    m = code_meta.hash_meta_v0("", "")
     assert len(m) == 32
     assert m.hex() == "af1349b9f5f9a1a6a0404dea36dcc9499bcb25c9adc112b7cc9a93cae41f3262"
 
@@ -15,7 +57,7 @@ def test_hash_meta_v0_extra_only():
 
 
 def test_hash_meta_v0_interleaved():
-    ma = code_meta.hash_meta_v0("", "")
+    ma = code_meta.hash_meta_v0("")
     mb = code_meta.hash_meta_v0("", "hello")
     assert ma[:4] == mb[:4]
     assert ma[4:8] == mb[8:12]
@@ -32,7 +74,24 @@ def test_code_meta_v0_extra_only_128_bits():
 
 
 def test_code_meta_v0_interleaved():
-    ma = code_meta.gen_meta_code_v0("", "")
+    ma = code_meta.gen_meta_code_v0("")
     mb = code_meta.gen_meta_code_v0("", "hello")
     assert ma == "AAA26E2JXH27TING"
     assert mb == "AAA26E2JXFSSZZN3"
+
+
+def test_trim_text():
+    multibyte_2 = "ü" * 128
+    trimmed = code_meta.trim_text(multibyte_2, 128)
+    assert 64 == len(trimmed)
+    assert 128 == len(trimmed.encode("utf-8"))
+
+    multibyte_3 = "驩" * 128
+    trimmed = code_meta.trim_text(multibyte_3, 128)
+    assert 42 == len(trimmed)
+    assert 126 == len(trimmed.encode("utf-8"))
+
+    mixed = "Iñtërnâtiônàlizætiøn☃💩" * 6
+    trimmed = code_meta.trim_text(mixed, 128)
+    assert 85 == len(trimmed)
+    assert 128 == len(trimmed.encode("utf-8"))
