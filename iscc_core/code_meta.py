fb71b1918849b509990ee9225b8ada38d677f2e0
Titusz Pan <titusz.pan@gmail.com>
2021-11-13 22:07:13 +0100

    Remove Meta-Code subtyping.

--- iscc_core/code_meta.py
+++ iscc_core/code_meta.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-The Meta-Code is the first component of a fully qualified ISCC. It is calculated as a
+The Meta-Code is the first component of a canonical ISCC. It is calculated as a
 similarity preserving hash from the metadata of a digital asset. The purpose of the
 Meta-Code is the discovery of digital assets with similar metadata or spelling mistakes.
 
-The metadata we supply to the algorithm is called *seed metadata*.
+The metadata supplied to the algorithm is called *seed metadata*.
 
 *Seed metadata* is composed of a `title` and an optional generic `extra`-field that
 contains descriptive, industry-sector or use-case specific metadata in textual or
@@ -12,7 +12,7 @@ binary form (e.g. file headers). We do not prescribe a particular schema.
 """
 from more_itertools import interleave, sliced
 from iscc_core.code_content_text import normalize_text
-from iscc_core.codec import MT, ST_MC, VS, encode_component
+from iscc_core.codec import MT, ST, ST_MC, VS, encode_component
 from iscc_core.utils import sliding_window
 from iscc_core.simhash import similarity_hash
 from iscc_core.options import opts
@@ -25,9 +25,9 @@ def gen_meta_code(title, extra=None, bits=opts.meta_bits):
     """Create an ISCC Meta-Code using the latest standard algorithm.
 
     Applications that generate ISCCs should prioritize explicitly passed `title`
-    information. If not available they shall try to extract a title form the the digital
-    asset itself. If extraction fails, the application shall resort to the filename
-    before falling back to an empty string.
+    information. If not available they should try to extract a title form the digital
+    asset itself. If extraction fails, the application should resort to a normalized
+    filename before falling back to an empty string.
 
     Optional additional metadata may be supplied via the `extra`-field.
     The input can be:
@@ -66,24 +66,21 @@ def gen_meta_code_v0(title, extra=None, bits=opts.meta_bits):
     title = normalize_text(title)
     title = trim_text(title, opts.meta_trim_title)
 
-    # 2. Normalize extra and set Meta-Code SubType
+    # 2. Normalize extra
     if extra in (None, ""):
         extra = None
-        stype = ST_MC.TEXT
     elif isinstance(extra, str):
         extra = normalize_text(extra)
         extra = trim_text(extra, opts.meta_trim_extra)
-        stype = ST_MC.TEXT
     elif isinstance(extra, bytes):
         extra = extra[: opts.meta_trim_extra]
-        stype = ST_MC.BYTES
     else:
         raise ValueError("parameter `extra` must be of type str or bytes!")
 
     digest = hash_meta_v0(title, extra)
     meta_code = encode_component(
         mtype=MT.META,
-        stype=stype,
+        stype=ST.NONE,
         version=VS.V0,
         length=bits,
         digest=digest,
@@ -93,24 +90,30 @@ def gen_meta_code_v0(title, extra=None, bits=opts.meta_bits):
 
 def hash_meta_v0(title, extra=None):
     # type: (str, Union[str,bytes,None]) -> bytes
-    """Calculate simmilarity preserving 256-bit hash digest for provided metadata.
+    """Calculate simmilarity preserving 256-bit hash digest from asset metadata.
 
     Textual input should be stripped of markup, normalized and trimmed before hashing.
     Json metadata should be normalized with
     [JCS](https://tools.ietf.org/id/draft-rundgren-json-canonicalization-scheme-00.html)
 
     !!! note
-        If the `extra` field is supplied we create separate hashes for title and extra
-        and interleave them in 32-bit chunks. The processign algorithms and the subtype
-        of the generated hash digest depends on the type the `extra` input.
-        If the input is a **text string** (str) the processing unit is a character
-        (possibly multibyte) and the resulting hash type is **ST_MC.TEXT (0)**. If the
-        input is **bytes** the processing is done bytewise and the resulting hash type
-        is **ST_MC.BYTES (1)**.
+        The processing algorithm depends on the type of the `extra` input.
+        If the `extra` field is supplied and non-empty, we create separate hashes for
+        `title` and `extra` and interleave them in 32-bit chunks:
+
+        - If the input is `None` or an empty `str`/`bytes` object the Meta-Hash will
+        be generated from the `title`-field only.
+
+        - If the `extra`-input is a non-empty **text** string (str) the string is
+        lower-cased and the processing unit is an utf-8 endoded character
+        (possibly multibyte). The resulting hash is interleaved with the `title`-hash.
+
+        - If the `extra`-input is a non-empty **bytes** object the processing is done
+        bytewise and the resulting hash is interleaved with the `title`-hash.
 
     :param str title: Title of the work manifested in the digital asset
     :param Union[str,bytes,None] extra: Additional metadata for disambiguation
-    :return: 256-bit simhash digest for Meta-Code of SubType ST_MC.TEXT or ST.MC_BYTES
+    :return: 256-bit simhash digest for Meta-Code
     :rtype: bytes
     """
     title = title.lower()
@@ -118,7 +121,7 @@ def hash_meta_v0(title, extra=None):
     title_hash_digests = [blake3(s.encode("utf-8")).digest() for s in title_n_grams]
     simhash_digest = similarity_hash(title_hash_digests)
 
-    if extra in (None, ""):
+    if extra in (None, "", b""):
         return simhash_digest
     else:
         # Augment with interleaved hash for extra metadata
