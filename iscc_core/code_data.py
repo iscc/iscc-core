18fdfbcceba67195fce376ac14324b456f429884
Titusz Pan <titusz.pan@gmail.com>
2021-11-14 15:34:40 +0100

    Add Data-Code schema and optional granular features.

--- iscc_core/code_data.py
+++ iscc_core/code_data.py
@@ -2,39 +2,49 @@
 """
 *A similarity perserving hash for binary data (soft hash).*
 """
-from typing import Optional
+from typing import List, Optional, Tuple
+from more_itertools import chunked
 from iscc_core.cdc import data_chunks
-from iscc_core.minhash import minhash_256
+from iscc_core.minhash import minhash_256, minhash_64
 from iscc_core import codec
-from iscc_core.codec import Data, Stream
+from iscc_core.codec import Data, Stream, encode_base64
 from iscc_core.options import opts
+from iscc_core.models import DataCode
 import xxhash
 
 
-def gen_data_code(stream, bits=opts.data_bits):
-    # type: (Stream, int) -> str
+def gen_data_code(stream, bits=opts.data_bits, granular=opts.data_granular):
+    # type: (Stream, int) -> DataCode
     """
     Create a similarity preserving ISCC Data-Code with the latest standard algorithm.
 
     :param stream: Input data stream.
     :param int bits: Bit-length of ISCC Data-Code (default 64).
+    :param bool granular: Calculate additional granular data-features
     :return: ISCC Data-Code
-    :rtype: str
+    :rtype: DataCode
     """
-    return gen_data_code_v0(stream, bits)
+    return gen_data_code_v0(stream, bits, granular)
 
 
-def gen_data_code_v0(stream, bits=opts.data_bits):
-    # type: (Stream, int) -> str
+def gen_data_code_v0(stream, bits=opts.data_bits, granular=opts.data_granular):
+    # type: (Stream, int) -> DataCode
     """
     Create an ISCC Data-Code with algorithm v0.
 
     :param stream: Input data stream.
     :param int bits: Bit-length of ISCC Data-Code (default 64).
-    :return: str
+    :param bool granular: Calculate additional granular data-features
+    :return: DataCode with properties: code, features, sizes
+    :rtype: DataCode
     """
+    features, sizes = None, None
+    result = hash_data_v0(stream, granular=granular)
+    if granular:
+        digest, features, sizes = result
+    else:
+        digest = result
 
-    digest = hash_data_v0(stream)
     data_code = codec.encode_component(
         mtype=codec.MT.DATA,
         stype=codec.ST.NONE,
@@ -42,17 +52,26 @@ def gen_data_code_v0(stream, bits=opts.data_bits):
         length=bits,
         digest=digest,
     )
-    return data_code
+    data_code_obj = DataCode(
+        code=data_code,
+    )
+
+    if granular:
+        data_code_obj.features = features
+        data_code_obj.sizes = sizes
+
+    return data_code_obj
 
 
-def hash_data_v0(stream):
-    # type: (Stream) -> bytes
+def hash_data_v0(stream, granular=opts.data_granular):
+    # type: (Stream) -> Union[bytes, Tuple[bytes, List[str], List[int]]]
     """
     Create a similarity preserving Data-Hash digest
 
     :param stream: Input data stream.
-    :return: Similarity preserving Data-Hash digest
-    :rtype: bytes
+    :param bool granular: Calculate additional granular data-features
+    :return: 256-bit data soft-hash (if granular: a tuple of digest, features, sizes)
+    :rtype: Union[bytes, Tuple[bytes, List[str], List[int]]]
     """
     hasher = DataHasherV0()
     data = stream.read(opts.cdc_read_size)
@@ -61,10 +80,17 @@ def hash_data_v0(stream):
         hasher.push(data)
         data = stream.read(opts.cdc_read_size)
 
-    return hasher.digest()
+    if granular is False:
+        return hasher.digest()
+    elif granular is True:
+        return hasher.digest(), hasher.features(), hasher.sizes()
+    else:
+        raise ValueError("parameter granular must be True or False")
 
 
 class DataHasherV0:
+    """Incremental Data-Hash generator."""
+
     def __init__(self, data=None):
         # type: (Optional[Data]) -> None
         self.chunk_features = []
@@ -75,27 +101,56 @@ class DataHasherV0:
 
     def push(self, data):
         # type: (Data) -> None
+        """Push data to the Data-Hash generator."""
         if self.tail:
             data = self.tail + data
 
-        for chunk in data_chunks(data, False):
+        for chunk in data_chunks(
+            data, utf32=False, avg_chunk_size=opts.data_avg_chunk_size
+        ):
             self.chunk_sizes.append(len(chunk))
             self.chunk_features.append(xxhash.xxh32_intdigest(chunk))
+            self.tail = chunk  # Last chunk may not be final
 
-        # Last chunk may not be final
-        self.tail = chunk
         self.chunk_features = self.chunk_features[:-1]
         self.chunk_sizes = self.chunk_sizes[:-1]
 
     def digest(self):
         # type: () -> bytes
-        chunk_features = self.chunk_features
-        chunk_sizes = self.chunk_sizes
+        """Calculate 256-bit minhash digest from feature hashes."""
+        self._finalize()
+        return minhash_256(self.chunk_features)
+
+    def features(self):
+        # type: () -> List[str]
+        """Calculate and encode granular feature hashes.
+
+        :return: Base64 encoded granular features (64-bit soft hashes)
+        :rtype: List[str]
+        """
+        self._finalize()
+        encoded_features = [
+            encode_base64(minhash_64(cf))
+            for cf in chunked(self.chunk_features, opts.data_granular_factor)
+        ]
+        return encoded_features
+
+    def sizes(self):
+        # type: () -> List[int]
+        """Calculate sizes of granular feature chunks
+
+        :return: List of sizes of granular features in number of bytes
+        :rtype: List[int]
+        """
+        self._finalize()
+        sizes = [sum(fh) for fh in chunked(self.chunk_sizes, opts.data_granular_factor)]
+        return sizes
+
+    def _finalize(self):
         if self.tail is not None:
-            chunk_features.append(xxhash.xxh32_intdigest(self.tail))
-            chunk_sizes.append(len(self.tail))
+            self.chunk_features.append(xxhash.xxh32_intdigest(self.tail))
+            self.chunk_sizes.append(len(self.tail))
             self.tail = None
-        return minhash_256(chunk_features)
 
 
 DataHasher = DataHasherV0
--- iscc_core/models.py
+++ iscc_core/models.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 """Schema of objects returned by ISCC processing algorithms"""
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 
+class DataCode(BaseModel):
+
+    code: str = Field(..., description="Data-Code in standard representation.")
+    features: Optional[List[str]] = Field(description="List of per datachunk hashes")
+    sizes: Optional[List[int]] = Field(description="Sizes of datachunks")
+
+
 class InstanceCode(BaseModel):
+
     code: str = Field(..., description="Instance-Code in standard representation.")
     datahash: str = Field(description="Multihash of digital asset (Blake3 by default.")
     filesize: int = Field(description="File size in bytes.")
