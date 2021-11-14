4ffd583e8c4b08b10013aa20c6500b6841875eea
Titusz Pan <titusz.pan@gmail.com>
2021-11-14 13:42:52 +0100

    Export more functions to toplevel.

--- iscc_core/__init__.py
+++ iscc_core/__init__.py
@@ -1,9 +1,25 @@
-__version__ = "0.1.1"
-from iscc_core.code_meta import gen_meta_code
-from iscc_core.code_content_text import gen_text_code
-from iscc_core.code_content_image import gen_image_code
-from iscc_core.code_content_audio import gen_audio_code
-from iscc_core.code_content_video import gen_video_code
-from iscc_core.code_data import gen_data_code
-from iscc_core.code_instance import gen_instance_code
+__version__ = "0.1.2"
+from iscc_core.code_meta import gen_meta_code, gen_meta_code_v0, hash_meta_v0
+from iscc_core.code_content_text import gen_text_code, gen_text_code_v0, hash_text_v0
+from iscc_core.code_content_image import (
+    gen_image_code,
+    gen_image_code_v0,
+    hash_image_v0,
+)
+from iscc_core.code_content_audio import (
+    gen_audio_code,
+    gen_audio_code_v0,
+    hash_audio_v0,
+)
+from iscc_core.code_content_video import (
+    gen_video_code,
+    gen_video_code_v0,
+    hash_video_v0,
+)
+from iscc_core.code_data import gen_data_code, gen_data_code_v0, hash_data_v0
+from iscc_core.code_instance import (
+    gen_instance_code,
+    gen_instance_code_v0,
+    hash_instance_v0,
+)
 from iscc_core.codec import Code, compose, decompose
