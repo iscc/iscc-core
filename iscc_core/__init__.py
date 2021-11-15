faf7f2f6b35964a5fa0db8e44b8c81f862e50762
Titusz Pan <titusz.pan@gmail.com>
2021-11-15 10:14:14 +0100

    Add more package top-level imports

--- iscc_core/__init__.py
+++ iscc_core/__init__.py
@@ -1,29 +1,46 @@
 __version__ = "0.1.2"
-from iscc_core.code_meta import gen_meta_code, gen_meta_code_v0, soft_hash_meta_v0
+from iscc_core.code_meta import (
+    gen_meta_code,
+    gen_meta_code_v0,
+    soft_hash_meta_v0,
+    MetaCode,
+)
 from iscc_core.code_content_text import (
     gen_text_code,
     gen_text_code_v0,
     soft_hash_text_v0,
+    ContentCodeText,
 )
 from iscc_core.code_content_image import (
     gen_image_code,
     gen_image_code_v0,
     soft_hash_image_v0,
+    ContentCodeImage,
 )
 from iscc_core.code_content_audio import (
     gen_audio_code,
     gen_audio_code_v0,
     soft_hash_audio_v0,
+    ContentCodeAudio,
 )
 from iscc_core.code_content_video import (
     gen_video_code,
     gen_video_code_v0,
     soft_hash_video_v0,
+    ContentCodeVideo,
 )
-from iscc_core.code_data import gen_data_code, gen_data_code_v0, soft_hash_data_v0
+from iscc_core.code_data import (
+    gen_data_code,
+    gen_data_code_v0,
+    soft_hash_data_v0,
+    DataCode,
+    DataHasher,
+)
 from iscc_core.code_instance import (
     gen_instance_code,
     gen_instance_code_v0,
     hash_instance_v0,
+    InstanceCode,
+    InstanceHasher,
 )
 from iscc_core.codec import Code, compose, decompose
