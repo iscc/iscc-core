89442bf8ad41993fb595d0993f9333602a531ebd
Titusz Pan <titusz.pan@gmail.com>
2021-12-05 14:02:24 +0100

    Add conformance test system.

---
+++ tests/build_test_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Build conformance `data.json` from `inputs.yaml`"""
+import yaml
+import json
+import pathlib
+import iscc_core
+from iscc_core.schema import BaseCode
+
+HERE = pathlib.Path(__file__).parent.absolute()
+INPUTS = HERE / "inputs.yaml"
+TEST_DATA = HERE / "data.json"
+
+
+def main():
+    with open(INPUTS, "rt", encoding="utf-8") as stream:
+        data = yaml.safe_load(stream)
+
+    for funcname, tests in data.items():
+        for testname, testdata in tests.items():
+            func = getattr(iscc_core, funcname)
+            args = testdata["inputs"]
+            result = func(*args)
+            if isinstance(result, BaseCode):
+                testdata["outputs"] = result.dict(exclude_unset=True, exclude_none=True)
+            else:
+                testdata["outputs"] = result
+    with open(TEST_DATA, "w", encoding="utf-8") as outf:
+        json.dump(data, outf, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_conformance.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Run conformance tests"""
+import pytest
+import json
+import pathlib
+import iscc_core
+
+
+HERE = pathlib.Path(__file__).parent.absolute()
+TEST_DATA = HERE / "data.json"
+
+
+def read_test_data():
+    with open(TEST_DATA, "rb") as stream:
+        data = json.load(stream)
+    flat = []
+    for func_name, tests in data.items():
+        func_obj = getattr(iscc_core, func_name)
+        for test_name, test_values in tests.items():
+            flat.append((func_obj, test_values["inputs"], test_values["outputs"]))
+    return flat
+
+
+@pytest.mark.parametrize("function,inputs,outputs", read_test_data())
+def test_conformance(function, inputs, outputs):
+    assert function(*inputs).dict(exclude_unset=True, exclude_none=True) == outputs
