909a58844aadb703abbca288b19886a9f62d8108
Titusz Pan <titusz.pan@gmail.com>
2021-12-09 16:13:30 +0100

    Support ISCC-ID generation from Instance-Code.

--- iscc_core/iscc_id.py
+++ iscc_core/iscc_id.py
@@ -6,17 +6,17 @@ The **ISCC-ID** is generated from a similarity-hash of the components of an
 minted. The similarity-hash is always at least 64-bits and optionally suffixed with a
 [`uvarint`](https://github.com/multiformats/unsigned-varint) endcoded
 `uniqueness counter`. The `uniqueness counter` is added and incremented only if the
-mint colides with a pre-existing **ISCC-ID minted** from the same blockchain from a
+mint colides with a pre-existing **ISCC-ID** minted from the same blockchain from a
 different **ISCC-CODE** or from an identical **ISCC-CODE** registered by a different
 signatory.
 """
 from iscc_core import simhash, codec
-from iscc_core.schema import ISCC_ID
+from iscc_core.schema import IsccID
 import uvarint
 
 
 def gen_iscc_id(chain, iscc_code, uc=0):
-    # type: (int, str, int) -> ISCC_ID
+    # type: (int, str, int) -> IsccID
     """
     Generate an ISCC-ID from an ISCC-CODE with uniqueness counter 'uc' with latest
     standard algorithm.
@@ -25,13 +25,13 @@ def gen_iscc_id(chain, iscc_code, uc=0):
     :param str iscc_code: The ISCC-CODE from which to mint the ISCC-ID.
     :param int uc: Uniqueness counter of ISCC-ID.
     :return: ISCC-ID
-    :rtype: ISCC_ID
+    :rtype: IsccID
     """
     return gen_iscc_id_v0(chain, iscc_code, uc)
 
 
 def gen_iscc_id_v0(chain_id, iscc_code, uc=0):
-    # type: (int, str, int) -> ISCC_ID
+    # type: (int, str, int) -> IsccID
     """
     Generate an ISCC-ID from an ISCC-CODE with uniqueness counter 'uc' with
     algorithm v0.
@@ -40,11 +40,10 @@ def gen_iscc_id_v0(chain_id, iscc_code, uc=0):
     :param str iscc_code: The ISCC-CODE from which to mint the ISCC-ID.
     :param int uc: Uniqueness counter for ISCC-ID.
     :return: ISCC-ID
-    :rtype: ISCC_ID
+    :rtype: IsccID
     """
     assert chain_id in list(codec.ST_ID), "Unregistered Chain-ID {}".format(chain_id)
-    digest = codec.decode_base32(iscc_code)
-    iscc_id_digest = soft_hash_iscc_id_v0(digest, uc)
+    iscc_id_digest = soft_hash_iscc_id_v0(iscc_code, uc)
     iscc_id_len = len(iscc_id_digest) * 8
     iscc_id = codec.encode_component(
         mtype=codec.MT.ID,
@@ -53,26 +52,38 @@ def gen_iscc_id_v0(chain_id, iscc_code, uc=0):
         length=iscc_id_len,
         digest=iscc_id_digest,
     )
-    return ISCC_ID(iscc=iscc_id)
+    return IsccID(iscc=iscc_id)
 
 
-def soft_hash_iscc_id_v0(digest, uc=0):
-    # type: (bytes, int) -> bytes
+def soft_hash_iscc_id_v0(iscc_code, uc=0):
+    # type: (str, int) -> bytes
     """
     Calculate ISCC-ID hash digest from ISCC-CODE digest with algorithm v0.
 
-    :param bytes digest: Byte-Digest of ISCC-CODE
+    :param str iscc_code: ISCC-CODE
     :param int uc: Uniqueness counter for ISCC-ID.
     :return: Digest for ISCC-ID without header but including uniqueness counter.
     :rtype: bytes
     """
-    components = codec.decompose(digest)
-    digests = [
-        c.header_bytes[:1] + c.hash_bytes[:7]
-        for c in components
-        if c.maintype != codec.MT.INSTANCE
-    ]
+    components = codec.decompose(iscc_code)
+    decoded = [codec.decode_base32(c) for c in components]
+    unpacked = [codec.read_header(d) for d in decoded]
+
+    digests = []
+
+    if len(unpacked) == 1 and unpacked[0][0] == codec.MT.INSTANCE:
+        # Special case if iscc_code is a singular Instance-Code
+        digests.append(decoded[0][:1] + unpacked[0][-1][:7])
+    else:
+        digests = []
+        for dec, unp in zip(decoded, unpacked):
+            if unp[0] == codec.MT.INSTANCE:
+                continue
+            # first byte of header + first 7 bytes of body
+            digests.append(dec[:1] + unp[-1][:7])
+
     iscc_id_digest = simhash.similarity_hash(digests)
+
     if uc:
         iscc_id_digest += uvarint.encode(uc)
     return iscc_id_digest
