61f177f6cfb93d61b66e376fb93734f63a18bf15
Titusz Pan <titusz.pan@gmail.com>
2021-12-24 15:33:34 +0100

    Add schema builder development tool.

---
+++ tests/build_json_schema.py
@@ -0,0 +1,19 @@
+"""Build `iscc.json` schema file from iscc_core.schema"""
+import iscc_core
+from os.path import dirname, abspath, join
+
+
+HERE = dirname(abspath(__file__))
+SCHEMA_PATH = join(HERE, "../iscc_core/iscc.json")
+
+
+def main():
+    """Generate iscc.json schema"""
+    from os.path import dirname, abspath, join
+
+    with open(SCHEMA_PATH, "wt", encoding="UTF-8") as outf:
+        outf.write(iscc_core.IsccCode.schema_json(indent=2))
+
+
+if __name__ == "__main__":
+    main()
