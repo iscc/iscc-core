616c93be0a6a842c0ebbef35448df6b0847d57de
Titusz Pan <titusz.pan@gmail.com>
2022-01-30 17:47:26 +0100

    Import full API to toplevel

--- iscc_core/__init__.py
+++ iscc_core/__init__.py
@@ -6,52 +6,27 @@ core_opts = options.CoreOptions()
 options_conformant = options.check_options(core_opts)
 
 
-from iscc_core import conformance
+# Import full api to toplevel
+from iscc_core.conformance import *
 from iscc_core.constants import *
+
 from iscc_core.simhash import *
 from iscc_core.minhash import *
 from iscc_core.wtahash import *
 from iscc_core.dct import *
 from iscc_core.cdc import *
-from iscc_core.iscc_code import (
-    gen_iscc_code,
-    gen_iscc_code_v0,
-)
-from iscc_core.iscc_id import (
-    gen_iscc_id,
-    gen_iscc_id_v0,
-)
-from iscc_core.code_meta import (
-    gen_meta_code,
-    gen_meta_code_v0,
-)
-from iscc_core.code_content_text import (
-    gen_text_code,
-    gen_text_code_v0,
-    collapse_text,
-)
-from iscc_core.code_content_image import (
-    gen_image_code,
-    gen_image_code_v0,
-    soft_hash_image_v0,
-)
-from iscc_core.code_content_audio import (
-    gen_audio_code,
-    gen_audio_code_v0,
-)
-from iscc_core.code_content_video import (
-    gen_video_code,
-    gen_video_code_v0,
-)
-from iscc_core.code_content_mixed import (
-    gen_mixed_code,
-    gen_mixed_code_v0,
-)
-from iscc_core.code_data import (
-    gen_data_code,
-    gen_data_code_v0,
-)
-from iscc_core.code_instance import gen_instance_code, gen_instance_code_v0, InstanceHasherV0
+
+from iscc_core.iscc_code import *
+from iscc_core.iscc_id import *
+from iscc_core.code_meta import *
+from iscc_core.code_content_text import *
+from iscc_core.code_content_image import *
+from iscc_core.code_content_audio import *
+from iscc_core.code_content_video import *
+from iscc_core.code_content_mixed import *
+from iscc_core.code_data import *
+from iscc_core.code_instance import *
+
 from iscc_core.codec import *
 from iscc_core.utils import *
-from iscc_core.models import Code
+from iscc_core.models import *
