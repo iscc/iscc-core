6739d5b145824d82b1f3da1d9522dfeaf251a569
Titusz Pan <titusz.pan@gmail.com>
2022-02-11 12:57:39 +0100

    Add v0 module as collection of conformance relevant functions

---
+++ iscc_core/v0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Collects stable v0 top level api into its own module"""
+from iscc_core.code_meta import gen_meta_code_v0, soft_hash_meta_v0
+from iscc_core.code_content_text import gen_text_code_v0, soft_hash_text_v0
+from iscc_core.code_content_image import gen_image_code_v0, soft_hash_image_v0
+from iscc_core.code_content_audio import gen_audio_code_v0, soft_hash_audio_v0
+from iscc_core.code_content_video import gen_video_code_v0, soft_hash_video_v0
+from iscc_core.code_content_mixed import gen_mixed_code_v0, soft_hash_codes_v0
+from iscc_core.code_data import gen_data_code_v0, soft_hash_data_v0, DataHasherV0
+from iscc_core.code_instance import gen_instance_code_v0, hash_instance_v0, InstanceHasherV0
+from iscc_core.iscc_code import gen_iscc_code_v0
+from iscc_core.iscc_id import gen_iscc_id_v0
