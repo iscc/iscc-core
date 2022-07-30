163bf2b016925978f6f3f0351190a149d9da52d8
Titusz Pan <titusz.pan@gmail.com>
2022-07-30 17:09:47 +0200

    Prepare for v0.2.12 development

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## [0.2.12] - Unreleased
+
 ## [0.2.11] - 2022-07-03
 - Add support for gracefull build failures
 
--- iscc_core/__init__.py
+++ iscc_core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 from iscc_core.options import core_opts, conformant_options
 
 # Import full api to toplevel
