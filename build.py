9a2ef691ae5a10be632602fc07ecf9ebad4abc52
Titusz Pan <titusz.pan@gmail.com>
2023-03-17 16:06:22 +0100

    Optional build

--- build.py
+++ build.py
@@ -9,52 +9,29 @@ $ cythonize -X language_level=3 -a -i ./iscc_core/simhash.py
 """
 try:
     from Cython.Build import cythonize, build_ext
+    from distutils.core import Distribution
 except ImportError:
     # dummy build function for poetry
     def build(setup_kwargs):
         pass
-
 else:
-
-    class build_ext_gracefull(build_ext):
-        def run(self):
-            try:
-                print("Trying to compile C accelerator modules")
-                super().run()
-                print("Successfully comiled C accelerator modules")
-            except Exception as e:
-                print(e)
-                print("********************************************************************")
-                print("Failed to compile C accelerator module, falling back to pure python.")
-                print("********************************************************************")
-
-        def build_extensions(self):
-            try:
-                print("Trying to compile C accelerator modules")
-                super().build_extensions()
-                print("Successfully comiled C accelerator modules")
-            except Exception as e:
-                print(e)
-                print("********************************************************************")
-                print("Failed to compile C accelerator module, falling back to pure python.")
-                print("********************************************************************")
-
     def build(setup_kwargs):
         try:
-            setup_kwargs.update(
-                dict(
-                    ext_modules=cythonize(
-                        [
-                            "iscc_core/cdc.py",
-                            "iscc_core/minhash.py",
-                            "iscc_core/simhash.py",
-                        ]
-                    ),
-                    cmdclass=dict(build_ext=build_ext_gracefull),
-                )
-            )
+            from Cython.Build import cythonize
+        except ImportError:
+            print("WARNING: Cython not installed. Skipping extension build.")
+            return setup_kwargs
+
+        extensions = cythonize(["iscc_core/cdc.py", "iscc_core/minhash.py", "iscc_core/simhash.py"])
+        distribution = Distribution({"name": "iscc_core", "ext_modules": extensions})
+        distribution.package_dir = "iscc_core"
+        try:
+            cmd = build_ext(distribution)
+            cmd.ensure_finalized()
+            cmd.run()
         except Exception as e:
             print(e)
             print("********************************************************************")
             print("Failed to compile C accelerator module, falling back to pure python.")
             print("********************************************************************")
+        return setup_kwargs
