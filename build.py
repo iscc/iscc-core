3af3d010fe429d076eb8a5d223c40e6cab8c8378
Titusz Pan <titusz.pan@gmail.com>
2023-03-17 16:24:12 +0100

    Don´t use custom ext_build

--- build.py
+++ build.py
@@ -13,27 +13,11 @@ try:
 except ImportError:
     # dummy build function for poetry
     def build(setup_kwargs):
-        pass
+        return setup_kwargs
 
 else:
 
     def build(setup_kwargs):
-        try:
-            from Cython.Build import cythonize
-        except ImportError:
-            print("WARNING: Cython not installed. Skipping extension build.")
-            return setup_kwargs
-
         extensions = cythonize(["iscc_core/cdc.py", "iscc_core/minhash.py", "iscc_core/simhash.py"])
-        distribution = Distribution({"name": "iscc_core", "ext_modules": extensions})
-        distribution.package_dir = "iscc_core"
-        try:
-            cmd = build_ext(distribution)
-            cmd.ensure_finalized()
-            cmd.run()
-        except Exception as e:
-            print(e)
-            print("********************************************************************")
-            print("Failed to compile C accelerator module, falling back to pure python.")
-            print("********************************************************************")
+        setup_kwargs.update(dict(ext_modules=extensions))
         return setup_kwargs
