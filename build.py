c739a025f7cf63cc092c215dbaa229cfddb39463
Titusz Pan <titusz.pan@gmail.com>
2023-11-29 22:09:05 +0100

    simplify build script

--- build.py
+++ build.py
@@ -7,53 +7,27 @@ $ cythonize -X language_level=3 -a -i ./iscc_core/cdc.py
 $ cythonize -X language_level=3 -a -i ./iscc_core/minhash.py
 $ cythonize -X language_level=3 -a -i ./iscc_core/simhash.py
 """
-try:
-    from Cython.Build import cythonize, build_ext
-except ImportError:
-    def build(setup_kwargs):  # type: (dict) -> None
-        pass
 
-else:
 
-    class build_ext_graceful(build_ext):
-        """Custom build extension class with error propagation."""
+def build(setup_kwargs):
+    # Check if Cython is available
+    try:
+        from Cython.Build import cythonize
+    except ImportError:
+        print("Cython is not enabled, skipping build.")
+        return
 
-        def run(self):
-            """Compile C accelerator modules and propagate errors."""
-            try:
-                print("Trying to compile C accelerator modules")
-                super().run()
-                print("Successfully compiled C accelerator modules")
-            except Exception as e:
-                print(f"Error during compilation: {e}")
-                raise  # Rethrow the exception to ensure it's captured by poetry
-
-        def build_extensions(self):
-            """Build extensions with error propagation."""
-            try:
-                print("Trying to compile C accelerator modules")
-                super().build_extensions()
-                print("Successfully compiled C accelerator modules")
-            except Exception as e:
-                print(f"Error during compilation: {e}")
-                raise  # Rethrow the exception
-
-    def build(setup_kwargs):  # type: (dict) -> None
-        """Configure setup kwargs for Cython compilation with error propagation."""
-        try:
-            setup_kwargs.update(
-                {
-                    "ext_modules": cythonize(
-                        [
-                            "iscc_core/cdc.py",
-                            "iscc_core/minhash.py",
-                            "iscc_core/simhash.py",
-                        ],
-                        language_level="3",
-                    ),
-                    "cmdclass": {"build_ext": build_ext_graceful},
-                }
-            )
-        except Exception as e:
-            print(f"Error in setup configuration: {e}")
-            raise  # Rethrow the exception
+    try:
+        setup_kwargs.update(
+            {
+                "ext_modules": cythonize(
+                    [
+                        "iscc_core/cdc.py",
+                        "iscc_core/minhash.py",
+                        "iscc_core/simhash.py",
+                    ]
+                ),
+            }
+        )
+    except Exception as e:
+        print(f"Warning: Cython build failed ({e}), using pure Python mode.")
