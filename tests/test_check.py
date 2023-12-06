b88baad025f77756f8c21e81466d86a8eea29ae0
Titusz Pan <titusz.pan@gmail.com>
2023-12-06 17:47:27 +0100

    Simplify native module check

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
-from iscc_core import check
 import iscc_core as ic
-import array
-
-
-def test_check_suffix():
-    assert check.suffix("hello.world") == "world"
-
-
-def test_check_isnativemodule():
-    assert check.isnativemodule(check) is False
-    assert check.isnativemodule(array) is True
 
 
 def test_check_turbo(turbo):
