21286d5512cdbf81b7b3f0bdad3d272c8e050f4c
Titusz Pan <titusz.pan@gmail.com>
2023-12-06 18:17:17 +0100

    Remove simhash from native check

--- iscc_core/check.py
+++ iscc_core/check.py
@@ -9,9 +9,9 @@ __all__ = ["turbo"]
 def turbo():
     # type: () -> bool
     """Check whether all optional cython extensions have been compiled to native modules."""
-    from iscc_core import cdc, minhash, simhash
+    from iscc_core import cdc, minhash
 
-    modules = (cdc, minhash, simhash)
+    modules = (cdc, minhash)
     for module in modules:
         module_file = inspect.getfile(module)
         if module_file.endswith(".py"):
