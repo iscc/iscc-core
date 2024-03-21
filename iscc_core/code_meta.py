7eb1fd07635d866dbd9b32b64e37ed6ba075b462
fbranciard <francoisbranciard@gmail.com>
2024-03-21 18:27:23 +0100

    add data-url canonicalization before hashing for json mime-type

--- iscc_core/code_meta.py
+++ iscc_core/code_meta.py
@@ -4,6 +4,7 @@ from more_itertools import interleave, sliced
 from blake3 import blake3
 from typing import Optional, Union
 from data_url import DataURL
+from ast import literal_eval
 import iscc_core as ic
 
 
@@ -65,6 +66,10 @@ def gen_meta_code_v0(name, description=None, meta=None, bits=ic.core_opts.meta_b
             # Data-URL expected
             durl = meta
             payload = DataURL.from_url(durl).data
+            durl_mime_type = DataURL.from_url(durl).mime_type
+            if "json" in durl_mime_type:
+                durl_data_eval = literal_eval(payload.decode('utf-8'))
+                payload = jcs.canonicalize(durl_data_eval)
             meta_code_digest = soft_hash_meta_v0(name, payload)
             metahash = ic.multi_hash_blake3(payload)
             metadata_value = durl
