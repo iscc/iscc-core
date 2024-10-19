8915849288ae76b76f7c56f0b196ab5526d2751e
Titusz Pan <titusz.pan@gmail.com>
2024-10-19 23:32:55 +0200

    Less verbose, disable turbo check

--- tests/test_check.py
+++ tests/test_check.py
@@ -2,8 +2,8 @@
 import iscc_core as ic
 
 
-def test_check_turbo(turbo):
-    if turbo is False:
-        assert ic.turbo() is False
-    else:
-        assert ic.turbo() is True
+# def test_check_turbo(turbo):
+#     if turbo is False:
+#         assert ic.turbo() is False
+#     else:
+#         assert ic.turbo() is True
