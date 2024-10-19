7176f34dcaa67ac640880fe9ac62479d5ff5f909
Titusz Pan <titusz.pan@gmail.com>
2024-10-19 18:27:43 +0200

    Re-enable test for binary extension check

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import iscc_core as ic
-import sys
-import pytest
 
 
-@pytest.mark.skipif(sys.version_info[:2] == (3, 12), reason="Needs investigation")
 def test_check_turbo(turbo):
     if turbo is False:
         assert ic.turbo() is False
